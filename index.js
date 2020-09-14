254589965896752cfc9a348ccfdb979f320100e4
Hans <hans@wireflare.com>
2020-09-13 22:51:12 -0700

    update to show more card info

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import {majorArcana} from './lib/major-arcana'
 export const drawMajor = () => {
   const major = new Map()
   majorArcana.map(card => major.set(card.name, {...card.number, ...card.image}))
+  console.log(major.get('Strength'))
   return major.get('Strength')
 }
