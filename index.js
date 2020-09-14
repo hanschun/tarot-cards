79ddf21bf6a87a7069f91234e3dcba6e76dafb2e
Hans <hans@wireflare.com>
2020-09-13 22:46:19 -0700

    change to return a card

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import {majorArcana} from './lib/major-arcana'
 
 export const drawMajor = () => {
   const major = new Map()
-  majorArcana.map(card => major.set(card.name, {...card}))
-  console.log(major.get('Strength'))
+  majorArcana.map(card => major.set(card.name, {...card.number, ...card.image}))
+  return major.get('Strength')
 }
